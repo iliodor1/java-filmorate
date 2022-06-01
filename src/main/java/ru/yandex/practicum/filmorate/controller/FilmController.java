f518996e5590f33e61ee44be89b61e680b5f3706
Eldar <eldar.gai@yandex.ru>
2022-06-01 14:13:35 +0500

    refactor: change field to private final

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @Slf4j
 public class FilmController {
-    FilmService filmService;
+    private final FilmService filmService;
 
     @Autowired
     public FilmController(FilmService filmService) {
