672473e5db6bb841b00d0d7af845d448a672f231
Eldar <eldar.gai@yandex.ru>
2022-06-23 14:05:30 +0500

    feat: add mpa and genres

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,10 +1,10 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Data
 public class Film {
@@ -13,14 +13,26 @@ public class Film {
     private String description;
     private LocalDate releaseDate;
     private Integer duration;
-    private Set<Long> likes = new HashSet<>();
+    private Mpa mpa;
 
-    public Film(Long id, String name, String description, LocalDate releaseDate, Integer duration) {
+    @JsonIgnore
+    private final Set<Long> likes = new HashSet<>();
+    private Set<Genre> genres;
+
+    public Film(Long id,
+                String name,
+                String description,
+                LocalDate releaseDate,
+                Integer duration,
+                Mpa mpa,
+                Set<Genre> genres) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.releaseDate = releaseDate;
         this.duration = duration;
+        this.mpa = mpa;
+        this.genres = genres;
     }
 
 }
