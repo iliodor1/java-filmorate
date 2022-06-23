205e7e0abe3d6249af27b20ece21f3b34baed3ca
Eldar <eldar.gai@yandex.ru>
2022-06-23 14:06:00 +0500

    feat: add genres

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.BadRequestException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/films")
 public class FilmController {
     private final FilmService filmService;
 
@@ -19,37 +19,37 @@ public class FilmController {
         this.filmService = filmService;
     }
 
-    @PostMapping
+    @PostMapping("/films")
     public Film addFilm(@RequestBody Film film) throws ValidationException {
         return filmService.addFilm(film);
     }
 
-    @PutMapping
+    @PutMapping("/films")
     public Film updateFilm(@RequestBody Film film) throws ValidationException {
         return filmService.updateFilm(film);
     }
 
-    @GetMapping
+    @GetMapping("/films")
     public List<Film> getFilms() {
         return filmService.getFilms();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/films/{id}")
     public Film getFilm(@PathVariable Long id) {
         return filmService.getFilm(id);
     }
 
-    @PutMapping("/{id}/like/{userId}")
+    @PutMapping("/films/{id}/like/{userId}")
     public void putLike(@PathVariable Long id, @PathVariable Long userId) {
         filmService.putLike(id, userId);
     }
 
-    @DeleteMapping("/{id}/like/{userId}")
+    @DeleteMapping("/films/{id}/like/{userId}")
     public void deleteLike(@PathVariable Long id, @PathVariable Long userId) {
         filmService.deleteLike(id, userId);
     }
 
-    @GetMapping("/popular")
+    @GetMapping("/films/popular")
     public List<Film> getPopular(@RequestParam(defaultValue = "10") int count) {
         if (count <= 0) {
             throw new BadRequestException("Параметр count имеет отрицательное значение.");
@@ -57,4 +57,13 @@ public class FilmController {
         return filmService.getPopular(count);
     }
 
+    @GetMapping("/genres/{id}")
+    public Genre get(@PathVariable int id){
+        return filmService.get(id);
+    }
+
+    @GetMapping("/genres")
+    public List<Genre> getAll(){
+        return filmService.getAll();
+    }
 }
