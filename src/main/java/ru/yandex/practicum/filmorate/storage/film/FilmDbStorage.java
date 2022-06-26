bfb0f57d24c2541a08eb9222875b6c08eb5bcd3f
Eldar <eldar.gai@yandex.ru>
2022-06-26 19:42:16 +0500

    refactor getFilm method

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -1,28 +1,29 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.film.genre.GenreStorage;
 import ru.yandex.practicum.filmorate.storage.mapper.FilmRowMapper;
 
 import java.sql.PreparedStatement;
-import java.util.*;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 
 @Component
 @Qualifier("dataBase")
+@AllArgsConstructor
 public class FilmDbStorage implements FilmStorage {
     private final JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    public FilmDbStorage(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
+    private final GenreStorage genreStorage;
 
     @Override
     public Film add(Film film) {
@@ -54,6 +55,7 @@ public class FilmDbStorage implements FilmStorage {
                 "    duration = ?,\n" +
                 "    mpa_id = ?" +
                 "WHERE id = ?";
+
         jdbcTemplate.update(sqlQuery,
                 film.getName(),
                 film.getDescription(),
@@ -70,27 +72,36 @@ public class FilmDbStorage implements FilmStorage {
         String sqlQuery = "SELECT *\n" +
                 "FROM films f\n" +
                 "LEFT JOIN mpa m ON m.id = f.mpa_id";
+
         return jdbcTemplate.query(sqlQuery, new FilmRowMapper());
     }
 
     @Override
     public Optional<Film> getFilm(Long id) {
-        String sqlQuery = "SELECT *\n" +
-                "FROM films f\n" +
-                "LEFT JOIN mpa m ON m.ID = f.mpa_id\n" +
-                "WHERE f.id = ?";
-        return jdbcTemplate.query(sqlQuery, new FilmRowMapper(), id).stream().findFirst();
+        String sqlQuery = "SELECT * FROM films f LEFT JOIN mpa m ON m.id = f.mpa_id WHERE f.id = ?";
+
+        Set<Genre> genres = genreStorage.getFilmGenres(id);
+        Optional<Film> film = jdbcTemplate.query(sqlQuery, new FilmRowMapper(), id)
+                .stream().findFirst();
+
+        if (film.isPresent() & !genres.isEmpty()) {
+            film.get().setGenres(genres);
+        }
+
+        return film;
     }
 
     @Override
     public void putLike(Film film, User user) {
         String sqlQuery = "INSERT INTO likes(film_id, user_id) VALUES (?, ?)";
+
         jdbcTemplate.update(sqlQuery, film.getId(), user.getId());
     }
 
     @Override
     public void deleteLike(Film film, User user) {
         String sqlQuery = "DELETE FROM likes WHERE film_id = ? AND user_id = ?";
+
         jdbcTemplate.update(sqlQuery, film.getId(), user.getId());
     }
 
@@ -102,6 +113,7 @@ public class FilmDbStorage implements FilmStorage {
                 "GROUP BY f.id " +
                 "ORDER BY COUNT(l.user_id) DESC " +
                 "LIMIT ?";
+
         return jdbcTemplate.query(sqlQuery, new FilmRowMapper(), count);
     }
 
